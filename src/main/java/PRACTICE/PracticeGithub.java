2f72963e843cba607f9ed18b3dc136440d4995fb
Nabamita Halder <Nabamita Halder@DESKTOP-URIUOEH>
2021-12-09 15:12:46 +0530

    changes done

--- src/main/java/PRACTICE/PracticeGithub.java
+++ src/main/java/PRACTICE/PracticeGithub.java
@@ -3,6 +3,7 @@ package PRACTICE;
 public class PracticeGithub {
 	public static void main(String [] args) {
 		System.out.println("pussing framework to github");
+		System.out.println("clonning");
 	}
 
 }
